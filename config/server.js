aa9ae9442d489e634ddcc26da6fadbcbd06d3ce7
Mahesh Ravi <mahesh_ravi@condenast.com>
2021-05-04 13:25:04 +0530

    Strapi setup completed

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26bd8407cdc771f445e1519ac3211e21'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d3c3f3a-1c53-4fa7-a6a4-f6d00d9c7794'
+};
